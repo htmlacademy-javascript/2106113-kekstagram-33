7161000caad8b44c9df7797fed118c74c3f35746
Keks <keks@htmlacademy.ru>
2024-11-25 15:36:07 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/thumbnails.js
@@ -0,0 +1,27 @@
+import { openFull } from './big-pictures';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const usersPicturesFragment = document.createDocumentFragment();
+
+const usersPicturesContainer = document.querySelector('.pictures');
+
+const renderPosts = (objects) => {
+  objects.forEach((object) => {
+    const {url, description, likes, comments} = object;
+    const photoElement = pictureTemplate.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = url;
+    photoElement.querySelector('.picture__img').alt = description;
+    photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
+
+    photoElement.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      openFull(object);
+    });
+    usersPicturesFragment.appendChild(photoElement);
+  });
+  usersPicturesContainer.appendChild(usersPicturesFragment);
+};
+
+export { renderPosts };
