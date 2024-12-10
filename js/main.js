305d2ec1373aa5d171d8468b19c7480cfc55205e
veldyatskikh <sveldyatskikh@gmail.com>
2024-12-10 11:30:14 +0300

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-import { photoObjects } from './data.js';
 import './big-pictures.js';
 import { renderPosts } from './thumbnails.js';
-import './form.js';
-import './form-validation.js';
-import './form-photo-edit.js';
+import {closeOverlay} from './form.js';
+import {setImgFormSubmit} from './form-validation.js';
+import './form-photo-scale.js';
 import './form-photo-effects.js';
-renderPosts(photoObjects);
+import {getData} from './api.js';
+getData(renderPosts);
+
+setImgFormSubmit(closeOverlay);
